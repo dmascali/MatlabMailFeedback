73d3a4661e54e8aa7397926909f170f8fd11d9ef
Daniele Mascali <danielemascali@gmail.com>
2018-08-24 18:46:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Matlab Mail Feedback
+
+A set of functions for receiving mail feedback from Matlab.
+
+- **sendstatus(to,verbose)**  
+    sends the exit status of a script/function to the mail addresses (specified in _to_). In case of failure the function that caused the error
+ will be attached. 
+ 
+- **sendmsg(to,subject,message,attachments)**  
+    uses matlab _sendmail_ function to send mail pre-compiled with a set of useful info (works as matlab _sendmail_). 
+
+## Installation
+1. Add the folder to you MATLAB path.
+2. Configure **sendsetup.m** with the info of the email address from which you wish to send matlab feedback  
+ - if you use a _gmail_ account, you only have to fill the fields _sender_mail_ and _password_   
+ - if you use another mail service, you will also have to change the SMTP parameters.
+
+## Usage
+**sendstatus(to,verbose)** must be placed as the **FIRST** line of code of a matlab
+ script/function.
+ 
+ - _to_ is either a string specifying a single address, or a cell array of
+   addresses.
+ - _verbose_ (optional): [0 1 2]. Selecet the "verbosity" of the attachements in case of failure:  
+           - 0: no attachement  
+           - 1: sends just the file that caused the error (default)  
+           - 2: sends all the stacked files  
+
+**sendmsg(to,subject,message,attachments)** works as matlab _sendmail_ (see: https://it.mathworks.com/help/matlab/ref/sendmail.html).
---
+++ sendsetup.m
@@ -0,0 +1,46 @@
+function sendsetup
+%Configuration file for setting up the SMTP mail server. 
+%
+% You have to specifiy the sender email and its password before using 
+% "sendstatus" or "sendmsg". The SMTP service preferences are already configured
+% for a GMAIL account. If you wish to use a different mail service, change
+% them accordingly. 
+%__________________________________________________________________________
+%
+% Author:
+%   Daniele Mascali
+%   Enrico Fermi Center, MARBILab, Rome
+%   August, 2018
+%   danielemascali@gmail.com
+
+try 
+    [sender_mail,password] = sendsetup_dm;
+catch
+    sender_mail = '';  %specify here the sender email (eg: danielemascali@gmail.com)
+    password = '';     %specify here the sender email password  
+end
+
+if isempty(sender_mail)
+    fname = mfilename;
+    fpath = mfilename('fullpath');
+    error(sprintf('You have to specify a sender email in %s.m.\nNB: %s is preconfigured for a GMAIL address.',fpath,fname));
+elseif isempty(password) 
+    fname = mfilename;
+    fpath = mfilename('fullpath');
+    error('You have to specify the password of the sender email in send_setup.m');
+end
+
+% The below configuration is for GMAIL, change the smtp server accordingly
+% to your mail provider. 
+setpref('Internet','E_mail',sender_mail);
+setpref('Internet','SMTP_Server','smtp.gmail.com');
+setpref('Internet','SMTP_Username',sender_mail);
+setpref('Internet','SMTP_Password',password);
+% for unix system:
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port','465');
+
+return
+end
