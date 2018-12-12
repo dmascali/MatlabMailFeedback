642bf9c4aa5f7492126cdbd97e879bf7c0fc5e23
Daniele Mascali <danielemascali@gmail.com>
2018-12-12 23:54:34 +0100

    improve time string

--- readsec.m
+++ readsec.m
@@ -19,21 +19,21 @@ time_string = [];
 
 if t >= 24*3600
     days = floor(t/(24*3600));
-    time_string = [time_string,num2str(days),' day(s), '];
+    time_string = [time_string,num2str(days),' day',plural(days),', '];
 end
 
 if t >= 3600
     hours = floor((t-days*(24*3600))/3600);
-    time_string = [time_string,num2str(hours),' hour(s), '];
+    time_string = [time_string,num2str(hours),' hour',plural(hours),', '];
 end
 
 if t >= 60
     mins = floor((t-days*(24*3600)-hours*(3600))/60);
-    time_string = [time_string,num2str(mins),' min, '];
+    time_string = [time_string,num2str(mins),' min and '];
 end
 
 sec = t-days*(24*3600)-hours*(3600)-mins*60;
-time_string = [time_string,sprintf('%2.3f s.',sec)];
+time_string = [time_string,sprintf('%2.3f s',sec)];
 
 if nargout == 0 
     disp([num2str(t,'%15.3f'),'s -> ',time_string]);
@@ -41,5 +41,10 @@ else
     varargout{1} = time_string;
 end
 
+return
+end
+
+function str = plural(n)
+if n == 1; str = ''; else; str = 's'; end
 return
 end
